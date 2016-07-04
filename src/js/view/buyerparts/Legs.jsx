1c1091dda59c783904155b6a04d01dfcf7ba7c4f
Joey <joey@starapple.nl>
2016-07-04 12:05:52 +0200

    Adds Symbol defs for stats and resultsOverlay [t: 1h]

--- src/js/view/buyerparts/Legs.jsx
+++ src/js/view/buyerparts/Legs.jsx
@@ -3,8 +3,10 @@ import component    from 'omniscient';
 import isEven       from '../util/isEven';
 
 const legPaths = [
-    'M105,203H60v-6h50v6h-5Zm2,0v4h5v-4h-5Zm2,4v4h5v-4h-5Zm2,4v4h5v-4h-5Zm2,4v5h5v-5h-5ZM60,203v17h5V203H60Z',
-    'M105,203H60v-6h50v6h-5Zm0,0v4h5v-4h-5Zm-3,4v4h5v-4h-5Zm-3,4v4h5v-4H99Zm-3,4v5h5v-5H96ZM60,203v4h5v-4H60Zm3,4v4h5v-4H63Zm3,4v4h5v-4H66Zm3,4v5h5v-5H69Z'
+    // 'M105,203H60v-6h50v6h-5Zm2,0v4h5v-4h-5Zm2,4v4h5v-4h-5Zm2,4v4h5v-4h-5Zm2,4v5h5v-5h-5ZM60,203v17h5V203H60Z',
+    // 'M105,203H60v-6h50v6h-5Zm0,0v4h5v-4h-5Zm-3,4v4h5v-4h-5Zm-3,4v4h5v-4H99Zm-3,4v5h5v-5H96ZM60,203v4h5v-4H60Zm3,4v4h5v-4H63Zm3,4v4h5v-4H66Zm3,4v5h5v-5H69Z'
+    'M105,203H60v-6h50v6h-5Zm-2,0v4h7v-4h-7Zm-3,4v4h7v-4h-7Zm-3,4v4h7v-4H97Zm-3,4v5h7v-5H94ZM60,203v4h7v-4H60Zm3,4v4h7v-4H63Zm3,4v4h7v-4H66Zm3,4v5h7v-5H69Z',
+    'M105,203H60v-6h50v6h-5Zm0,0v4h7v-4h-7Zm2,4v4h7v-4h-7Zm2,4v4h7v-4h-7Zm2,4v5h7v-5h-7ZM60,203v17h5V203H60Z'
 ];
 
 const getLegs = (index, className) => (
